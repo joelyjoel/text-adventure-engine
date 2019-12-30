6a24054fcc0c2284350f8232a1f99130166e4d3f
Joel Plowright <joelyjoel@protonmail.com>
2019-12-30 06:35:30 +0000

    toSnakeCase()

--- src/util/toCamelCase.ts
+++ src/util/toCamelCase.ts
@@ -3,3 +3,7 @@ export function toCamelCase(...strings:string[]) {
     return str.split(/\s/).map(word => word[0].toUpperCase() + word.slice(1)).join('')
   }).join('')
 }
+
+export function toSnakeCase(...strings:string[]) {
+  return strings.map(str => str.replace(/\s+/g, '_')).join('_')
+}
