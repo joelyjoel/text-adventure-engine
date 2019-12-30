fe6e37c6a367691918225426df0fa46357ed66a3
Joel Plowright <joelyjoel@protonmail.com>
2019-12-30 06:34:20 +0000

    Unique adjective and noun symbols

--- src/Adjective.ts
+++ src/Adjective.ts
@@ -1,16 +1,20 @@
 import { AdjectivePredicate } from "./linking/AdjectivePredicate";
+import { toSnakeCase } from "./util/toCamelCase";
 
 /** Class for storing an adjecitve inside a `Dictionary` object. */
 export class Adjective {
   str: string;
   phrasal: boolean;
   predicate: AdjectivePredicate;
+  readonly symbol: string;
 
   constructor(str:string) {
     this.str = str;
     this.phrasal = /\s/.test(str);
 
     this.predicate = new AdjectivePredicate(this);
+    
+    this.symbol = toSnakeCase(this.str);
   }
 
   get lastWord() {
--- src/Noun.ts
+++ src/Noun.ts
@@ -1,15 +1,20 @@
+
 import { NounPredicate } from "./linking/NounPredicate";
+import { toSnakeCase } from "./util/toCamelCase";
 
 export class Noun {
   str: string;
   phrasal: boolean;
   predicate: NounPredicate
+  readonly symbol: string;
 
   constructor(str:string) {
     this.str = str;
     this.phrasal = /\s/.test(str);
 
     this.predicate = new NounPredicate(this);
+
+    this.symbol = toSnakeCase(str);
   }
 
   get lastWord() {
