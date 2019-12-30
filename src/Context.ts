2129e371b275f0cac05fcfd5881ca9a364c5a7db
Joel Plowright <joelyjoel@protonmail.com>
2019-12-30 08:21:23 +0000

    contexts accept linking matrixs

--- src/Context.ts
+++ src/Context.ts
@@ -1,15 +1,34 @@
 import { Dictionary } from "./Dictionary";
 import { TruthTable } from "./logic/TruthTable";
 import { Entity } from "./logic";
+import { SyntaxLogicLinkingMatrix } from "./linking/SyntaxLogicLinkingMatrix";
 
 export class Context {
   dictionary: Dictionary;
   truthTable?: TruthTable;
+  linkingMatrix: SyntaxLogicLinkingMatrix;
 
   speaker: Entity;
   listener: Entity;
 
-  constructor(dictionary:Dictionary, truthTable?:TruthTable) {
+  constructor(
+    dictionaryOrLinking:Dictionary|SyntaxLogicLinkingMatrix, 
+    truthTable?:TruthTable
+  ) {
+    let dictionary:Dictionary, linkingMatrix:SyntaxLogicLinkingMatrix;
+    if(dictionaryOrLinking instanceof Dictionary) {
+      dictionary = dictionaryOrLinking;
+      linkingMatrix = new SyntaxLogicLinkingMatrix({dictionary});
+    } else if(dictionaryOrLinking instanceof SyntaxLogicLinkingMatrix) {
+      linkingMatrix = dictionaryOrLinking;
+      if(linkingMatrix.dictionary)
+        dictionary = linkingMatrix.dictionary;
+      else
+        throw "Cannot create a Context from a SyntaxLogicLinkingMatrix that has no Dictionary."
+    } else
+      throw "Something bad happened."
+
+    this.linkingMatrix = linkingMatrix;
     this.dictionary = dictionary;
     this.truthTable = truthTable;
   }
