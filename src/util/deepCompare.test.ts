aaed1af7e5ecadf4eaecd778d3ebbe9be57c73fc
Joel Plowright <joelyjoel@protonmail.com>
2020-10-07 02:03:46 +0100

    Deep object comparison util function

---
+++ src/util/deepCompare.test.ts
@@ -0,0 +1,35 @@
+import {deepCompare} from './deepCompare';
+
+describe('Testing deepCompare function for consistency', () => {
+  // Should return `true`
+  test.each([
+    [0, 0],
+    [69, 69],
+    ['hello', 'hello'],
+    [null, null],
+    [undefined, undefined],
+    [{a:5}, {a:5}],
+    [{a:5, b:6}, {b:6, a:5}],
+    [[1,2,3], [1,2,3]],
+    [
+      { article: 'the', adjectives: [ 'red' ], noun: 'truck', plural: false },
+      { article: 'the', adjectives: [ 'red' ], noun: 'truck', plural: false }
+    ],
+  ])('%o = %o', (a:any, b:any) => {
+    expect(deepCompare(a, b)).toBe(true);
+    expect(deepCompare(b, a)).toBe(true);
+  });
+
+  // Should return `false`
+  test.each([
+    [7, 6],
+    [1, 0.999],
+    ['hello', 'goodbye'],
+    [{a: 5}, {b:5}],
+    [undefined, null],
+    [undefined, 0],
+  ])('%o != %o', (a:any, b:any) => {
+    expect(deepCompare(a, b)).toBe(false);
+    expect(deepCompare(b, a)).toBe(false);
+  });
+})
---
+++ src/util/deepCompare.ts
@@ -0,0 +1,20 @@
+export function deepCompare(a:any, b:any):boolean {
+  if(a instanceof Object && b instanceof Object) {
+    // Check they have the same keys
+    const aKeys = Object.keys(a);
+    const bKeys = Object.keys(b);
+    if(!aKeys.every(key => bKeys.includes(key)))
+      return false;
+    if(!bKeys.every(key => aKeys.includes(key)))
+      return false;
+
+    // Check values match
+    for(let key in a)
+      if(!deepCompare(a[key], b[key]))
+        return false;
+
+    // Otherwise,
+    return true;
+  } else
+    return a === b;
+}
